dfd550a93c0f535558d4af638c59e9ef35ac8413
Tesfaye Gari <TGari@chemtrec.com>
2020-04-11 14:07:34 -0400

    Class 4-11-2020

--- repo/March2020SPFx/src/webparts/readingList/ReadingListWebPart.ts
+++ repo/March2020SPFx/src/webparts/readingList/ReadingListWebPart.ts
@@ -6,11 +6,12 @@ import {
   PropertyPaneToggle
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { PropertyFieldListPicker, PropertyFieldListPickerOrderBy } from '@pnp/spfx-property-controls/lib/PropertyFieldListPicker';
-import { escape } from '@microsoft/sp-lodash-subset';
-
-import styles from './ReadingListWebPart.module.scss';
-
+import {
+  PropertyFieldListPicker,
+  PropertyFieldListPickerOrderBy
+} from '@pnp/spfx-property-controls/lib/PropertyFieldListPicker';
+import { SPComponentLoader } from "@microsoft/sp-loader";
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 
 export interface IReadingListWebPartProps {
   description: string;
@@ -19,24 +20,56 @@ export interface IReadingListWebPartProps {
   lists: string | string[]; // Stores the list ID(s)
 }
 
+interface List {
+  Title: string;
+  ItemCount: number;
+}
+
 export default class ReadingListWebPart extends BaseClientSideWebPart<IReadingListWebPartProps> {
 
   public render(): void {
+    //_api/web/lists?$filter=Hidden eq false&$select=Title,ItemCount&$top=3
+    SPComponentLoader.loadCss('https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
     this.domElement.innerHTML = `
-      <div class="${ styles.readingList}">
-        <div class="${ styles.container}">
-          <div class="${ styles.row}">
-            <div class="${ styles.column}">
-              <span class="${ styles.title}">Welcome to SharePoint!</span>
-                  <p class="${ styles.subTitle}">Customize SharePoint experiences using Web Parts.</p>
-                  <p class="${ styles.description}">${escape(this.properties.description)}</p>
-                  <a href="http://mtmconsultinggroup.com" class="${ styles.button}">
-                    <span class="${ styles.label}">Learn more about MTM</span>
-                  </a>
-            </div>
-          </div>
-        </div>
-      </div>`;
+    <h1>All Lists you you have under your site</h1>
+    <div class="list-group list-group-flush" id="restData">
+      
+    </div>
+        `;
+    this.readLists();
+  }
+
+  private readLists() {
+    //this.context.spHttpClient.get()
+    this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists?$filter=Hidden eq false&$select=Title,ItemCount&$top=${this.properties.slideValue}`,
+      SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse): Promise<{ value: List[] }> => {
+        return response.json();
+      })
+      .then((response: { value: List[] }): void => {
+        this.updateStatus(`Successfully loaded ${response.value.length} items`, response.value);
+      }, (error: any): void => {
+        this.updateStatus('Loading all items failed with error: ' + error);
+      });
+  }
+  private updateStatus(status: string, items: List[] = []): void {
+    //this.domElement.querySelector('.status').innerHTML = status;
+    this.updateItemsHtml(items);
+  }
+
+  private updateItemsHtml(items: List[]): void {
+    console.log('Lists are ', items);
+    // let listHtml = '';
+    // for(let list of items){
+    //   listHtml += `
+    //   <a href="#" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">
+    //     ${list.Title}<span class="badge badge-primary badge-pill">${list.ItemCount}</span>
+    //   </a>`;
+    // }
+    // this.domElement.querySelector('#restData').innerHTML = listHtml;
+    this.domElement.querySelector('#restData').innerHTML = items.map(item => `<a href="#" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">
+                  ${item.Title}<span class="badge badge-primary badge-pill">${item.ItemCount}</span>
+                </a>`).join("");
   }
 
   protected get disableReactivePropertyChanges(): boolean {
@@ -72,13 +105,12 @@ export default class ReadingListWebPart extends BaseClientSideWebPart<IReadingLi
                   context: this.context,
                   onGetErrorMessage: null,
                   deferredValidationTime: 0,
-                  key: 'listPickerFieldId',
-                  multiSelect: true
+                  key: 'listPickerFieldId'
                 }),
                 PropertyPaneSlider('slideValue', {
                   label: "Max Lists",
                   min: 5,
-                  max: 20,
+                  max: 50,
                   value: 5,
                   showValue: true,
                   step: 1
